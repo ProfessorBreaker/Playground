777c4b39c27efb3947110ebedbaf44036b863f45
Professor Breaker <jbreak1@ju.edu>
2025-09-05 13:56:37 -0400

    Commit

---
+++ WorldTrotter(2)/WorldTrotter(2)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter(2)
+//
+//  Created by P.Breaker on 9/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorldTrotter(2)/WorldTrotter(2)Tests/WorldTrotter_2_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTrotter_2_Tests.swift
+//  WorldTrotter(2)Tests
+//
+//  Created by P.Breaker on 9/4/25.
+//
+
+import Testing
+@testable import WorldTrotter_2_
+
+struct WorldTrotter_2_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
